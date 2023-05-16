4bedad7c29e1f64a9359c2122d0b0d534e049ef4
SanielDamuel <114606891+slemsz@users.noreply.github.com>
2023-05-16 09:54:49 -0400

    updated with script

--- Refrigador/src/main/java/com/mycompany/app/App.java
+++ Refrigador/src/main/java/com/mycompany/app/App.java
@@ -1,14 +1,19 @@
 package com.mycompany.app;
 
 import com.google.gson.Gson;
+import java.io.IOException;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.net.URI;
+import java.util.Scanner;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
     private static final String ENDPOINT_MEAL = "https://www.themealdb.com/api/json/v1/1";
     private static final String CONFIG_PATH = "resources/config.properties";
 
@@ -21,8 +26,38 @@ public class App
     /**  Google {@code Gson} object for parsing JSON-formatted strings. */
     Gson gson = new Gson();
 
-    public static void main( String[] args )
-    {
+    /**
+     * fetchString Returns the response body string data from a URI.
+     * @param uri location of desired content
+     * @return response body string
+     * @throws IOException if an I/O error occurs when sending or receiving
+     * @throws InterruptedException if the HTTP client's {@code send} method is interrupted.
+     **/
+    public static String fetchString(String url) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create(url))
+            .GET()
+            .build();
+        HttpResponse<String> response = HTTP_CLIENT.send(request, BodyHandlers.ofString());
+        final int statusCode = response.statusCode();
+        if (statusCode != 200) throw new IOException("response status code not 200: %d" + statusCode);
+        System.out.println("\n" + response.statusCode());
+        return response.body().trim();
+    }
+
+    /**
+     * userInterface allows the user to interact with the program to allow to custom modification
+     * of inquiries formatted and show in the CLI
+     **/
+    public void userInterface() {
+        // Create a Scanner object
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(input);
+        scanner.close();
+    }
+
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
