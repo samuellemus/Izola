f3f40f4f57842a3e0b0a07f94d73b53463c75249
SanielDamuel <114606891+slemsz@users.noreply.github.com>
2023-05-18 09:52:04 -0400

    updated with script

--- Izola_1/run.sh
+++ Izola_1/run.sh
@@ -5,9 +5,10 @@
 # java -cp target/Refrigador-1.0-SNAPSHOT.jar mycompany.app.AppDriver
 # mvn clean
 
-
+javac -d bin -cp bin:resources/gson-2.10.1.jar src/main/java/mycompany/app/CustomJsonObject.java
 javac -d bin -cp bin:resources/gson-2.10.1.jar src/main/java/mycompany/app/ResponseObject.java
 javac -d bin -cp bin:resources/gson-2.10.1.jar src/main/java/mycompany/app/AppUtils.java
 javac -d bin -cp bin:resources/gson-2.10.1.jar src/main/java/mycompany/app/App.java
-java -cp bin:resources/gson-2.10.1.jar mycompany.app.App
 
+# java -cp bin:resources/gson-2.10.1.jar mycompany.app.App
+java -cp bin:resources/gson-2.10.1.jar mycompany.app.CustomJsonObject
