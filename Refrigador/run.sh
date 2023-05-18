b6b2916c9b4799bbefc6e58ac8755d05cb18cdca
SanielDamuel <114606891+slemsz@users.noreply.github.com>
2023-05-18 09:48:08 -0400

    updated with script

--- Refrigador/run.sh
+++ Refrigador/run.sh
@@ -9,6 +9,5 @@
 javac -d bin -cp bin:resources/gson-2.10.1.jar src/main/java/mycompany/app/ResponseObject.java
 javac -d bin -cp bin:resources/gson-2.10.1.jar src/main/java/mycompany/app/AppUtils.java
 javac -d bin -cp bin:resources/gson-2.10.1.jar src/main/java/mycompany/app/App.java
-
 java -cp bin:resources/gson-2.10.1.jar mycompany.app.App
 
--- git-refresh.sh
+++ git-refresh.sh
@@ -2,3 +2,8 @@
 git add .
 git commit -m "updated with script"
 git push origin main
+touch test.txt
+rm -rf test.txt
+git add .
+git commit -m "updated with script"
+git push origin main
